66b4f4a08135a11cee8ae9fafa3e730af35536f1
Kila67 <sirixay.phetnouvong@gmail.com>
2023-01-23 14:53:54 +0100

    usage de props

---
+++ src/App/components/ui/button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Button=()=>{
+    return <Button className='Button'>benjamin</Button>
+}
+
+export default Button;
+
+
+// function Button(){
+//     return <Button className='Button'>benjamin</Button>
+// }
