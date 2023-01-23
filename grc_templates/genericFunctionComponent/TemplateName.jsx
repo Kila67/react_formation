584cc4c8abe7aa4666486c65ea10ee48012aeec4
Kila67 <sirixay.phetnouvong@gmail.com>
2023-01-23 17:37:02 +0100

    creation template et ajout composant Header Footer

---
+++ grc_templates/genericFunctionComponent/TemplateName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './TemplateName.module.css'
+
+const TemplateName = (props) => {
+
+    return <div className={style.TemplateName} data-testid="TemplateName">
+        TemplateName
+    </div>
+};
+
+export default TemplateName;
---
+++ src/App/components/ui/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = (props) => {
+
+    return <div className={style.Footer} data-testid="Footer">
+        Footer
+    </div>
+};
+
+export default Footer;
